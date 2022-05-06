d6e5447d526ff2f90520cb1db06aad8a73da566d
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-05-06 08:22:41 +0300

    updated object constructor fun

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let js = "amazing";
+
+let results = 40 + 8 + 23 - 10;
+console.log(results);
+
+//declaring a variable
+let firstName = `Jonas`;
+console.log(firstName);
+
+// practice assignment 1
+
+let country = `Romania`;
+let continent = `Europe`;
+let population = 19000000;
+
+console.log(country, continent, population);
